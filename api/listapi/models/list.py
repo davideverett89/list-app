b5235c4ceda0e9b4ce3fb1c36969e47cbe5111ef
David Everett <davideverett1989@gmail.com>
2021-11-20 13:16:09 -0600

    Added id auto field.

--- api/listapi/models/list.py
+++ api/listapi/models/list.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class List(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
--- api/listapi/models/list_item.py
+++ api/listapi/models/list_item.py
@@ -2,6 +2,7 @@ from django.db import models
 from .list import List
 
 class ListItem(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     list = models.ForeignKey(List, on_delete=models.CASCADE, related_name='list')
