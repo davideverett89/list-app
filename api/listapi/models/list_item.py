8e9c3c3f97576bd70074b510f2a4104f9101de8c
David Everett <davideverett1989@gmail.com>
2021-11-21 01:54:04 -0600

    Done.

--- api/listapi/models/list_item.py
+++ api/listapi/models/list_item.py
@@ -4,7 +4,7 @@ from .list import List
 class ListItem(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True, null=True)
     list = models.ForeignKey(List, on_delete=models.CASCADE, related_name='list')
     
     def __str__(self):
